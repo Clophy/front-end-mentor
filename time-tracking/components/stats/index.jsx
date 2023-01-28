d35fa1c62f57eb52beee26bfcad3d01ea3ee7c50
Clophy <barangenez@hotmail.com>
2023-01-28 03:20:22 +0300

    Update index.jsx

--- time-tracking/components/stats/index.jsx
+++ time-tracking/components/stats/index.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Data from '@/mock/data.json'
 import Image from 'next/image';
 
-function Stats({tab, setTab}) {
+function Stats({tab}) {
 const data = Data;
-
+let baran;
+if(tab===0){ baran = "daily"}
+else if(tab===1){ baran = "weekly"}
+else if(tab===2){ baran = "monthly"}
   return (
     <div>
         
@@ -20,6 +23,7 @@ const data = Data;
                 <p>{e.title}</p>
                 <span><Image src={"/icon-ellipsis.svg"} width={14} height={14} alt="sa" /></span>
                 </div>
+                {console.log(baran)}
                 <div className='statsTime'>{e.timeframes.daily.current}hrs</div>
                 <p className='statsLast'>last week - {e.timeframes.daily.previous}hrs</p>
             </div>
