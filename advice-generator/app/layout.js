568dcc7925712ec51c18f138fd9ab7c6f024ffed
Clophy <barangenez@hotmail.com>
2023-01-29 16:01:30 +0300

    a
    a

---
+++ advice-generator/app/layout.js
@@ -0,0 +1,15 @@
+import '@/styles/reset.css'
+import '@/styles/global.css'
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+      {children}
+      </body>
+    </html>
+  )
+}
